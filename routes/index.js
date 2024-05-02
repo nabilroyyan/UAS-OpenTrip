760004ba9f17d3ec525afdb9bf0ba32efbc606ad
subhanfauzan <ariel1181987@gmail.com>
2024-05-02 10:32:31 +0700

    Push 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
+
+router.get("/login", function (req, res, next) {
+  res.render("auth/login");
+});
+router.get("/register", function (req, res, next) {
+  res.render("auth/register");
+});
+router.get("/paket", function (req, res, next) {
+  res.render("paket");
 });
 
 module.exports = router;
