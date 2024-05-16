69e5ea02e667ff882004f3069aca50f5afdf5c90
subhanfauzan <ariel1181987@gmail.com>
2024-05-16 10:14:52 +0700

    form paket

--- routes/admin.js
+++ routes/admin.js
@@ -8,5 +8,8 @@ router.get("/", function (req, res, next) {
 router.get("/paket", function (req, res, next) {
   res.render("admin/paket");
 });
+router.get("/tambahpaket", function (req, res, next) {
+  res.render("admin/tambahpaket");
+});
 
 module.exports = router;
