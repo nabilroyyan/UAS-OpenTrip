73a2cd1b6a4a23e9fee8d6d3f1f3ffc9f247a2a3
subhanfauzan <ariel1181987@gmail.com>
2024-05-16 09:45:47 +0700

    paket

--- routes/admin.js
+++ routes/admin.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   res.render("admin/index");
 });
+router.get("/paket", function (req, res, next) {
+  res.render("admin/paket");
+});
 
 module.exports = router;
