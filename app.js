625b1566ccf7beeda0c5698aad929a926bb7c080
subhanfauzan <ariel1181987@gmail.com>
2024-05-22 22:45:36 +0700

    fix login register

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var flash = require("express-flash");
 var session = require("express-session");
-const MemoryStore = require('session-memory-store')(session);
-const bodyParser = require('body-parser');
-
-
+const MemoryStore = require("session-memory-store")(session);
+const bodyParser = require("body-parser");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -16,8 +14,7 @@ var adminRouter = require("./routes/admin");
 var wisataRouter = require("./routes/wisata");
 var paketRouter = require("./routes/paket");
 
-const { strict } = require('assert');
-
+const { strict } = require("assert");
 
 var app = express();
 
@@ -27,25 +24,26 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session({
-  cookie:{
-   maxAge: 60000000000,
-   secure: false,
-   httpOnly: true,
-   sameSite: 'strict',
-   //domain: domainkkitananti.com',
-  },
-  store: new MemoryStore(),
-  saveUninitialized: true,
-  resave: false,
-  secret: 'secret' 
- }));
+app.use(
+  session({
+    cookie: {
+      maxAge: 60000000000,
+      secure: false,
+      httpOnly: true,
+      sameSite: "strict",
+      //domain: domainkkitananti.com',
+    },
+    store: new MemoryStore(),
+    saveUninitialized: true,
+    resave: false,
+    secret: "secret",
+  })
+);
 
 app.use(flash());
 
